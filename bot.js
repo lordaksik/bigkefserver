d5fd0c83f540393cdcc47b9ee48e84d62c7d9d07
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-06-15 11:02:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ if (result2 === 3)
   score_dealer = data.items.results[i].results.score_dealer
   score_player = data.items.results[i].results.score_player
    console.log("игрок " + score_player + " "+ score_dealer +" дилер")
-console.log(typeoff(score_player))
+console.log(typeof score_player))
   if ((score_player ===11) || (score_dealer ===11) || (score_player ===12) || (score_dealer ===12) ||
 (score_player ===13) || (score_dealer ===13) || (score_player ===14) && (score_dealer ===14))
       { fir = fir + 1;}
